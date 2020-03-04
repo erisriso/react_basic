ad1e00dfa4cf97964e258a0f5a041a3c4c9b9151
ERIS RISO <erisriso@gmail.com>
2020-03-04 23:02:34 +0700

    pertama

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+//import Biodata from './components/Biodata';
+//import NewBiodata from './components/NewBiodata';
+import C4 from "./components/Component4";
+class App extends Component{
+  render(){
+    return (
+      <C4/>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+
 
 export default App;
